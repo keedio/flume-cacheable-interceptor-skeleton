69144718648a31963c9648738ad2d0c93ae7999f
Emmanuelle Raffenne <eraffenne@keedio.com>
2015-02-27 14:17:21 +0100

    Reformat code

--- src/main/java/org/apache/flume/interceptor/CacheableInterceptor.java
+++ src/main/java/org/apache/flume/interceptor/CacheableInterceptor.java
@@ -10,43 +10,42 @@ import org.apache.flume.interceptor.service.ICacheService;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-/** *
+/**
  * Implements a cacheable interceptor
  *
  * @see <a href="http://docs.spring.io/spring/docs/current/spring-framework-reference/html/cache.html">Spring Cache docs</a>
- *
  */
 
 public class CacheableInterceptor implements Interceptor {
     private ApplicationContext context;
     private ICacheService<Event> service;
 
-	public CacheableInterceptor() {
+    public CacheableInterceptor() {
         // Get Spring context
         this.context = new ClassPathXmlApplicationContext("beans.xml");
-	}
-
-	@Override
-	public void initialize() {
-        // Get cache service instance
-        service = context.getBean( ICacheService.class );
     }
 
-	@Override
-	public Event intercept(Event event) {
+    @Override
+    public void initialize() {
+        // Get cache service instance
+        service = context.getBean(ICacheService.class);
+    }
+
+    @Override
+    public Event intercept(Event event) {
         // Edit the FlumeCacheService.intercept method to implement
         // the transformation you want to cache.
         return service.intercept(event);
-	}
+    }
 
-	@Override
+    @Override
     public List<Event> intercept(List<Event> events) {
         List<Event> intercepted = new LinkedList<Event>();
         for (Event e : events) {
             intercepted.add(intercept(e));
         }
         return intercepted;
-	}
+    }
 
     ApplicationContext getContext() {
         return context;
@@ -57,16 +56,16 @@ public class CacheableInterceptor implements Interceptor {
     }
 
     @Override
-	public void close() {
-	}
+    public void close() {
+    }
 
-	public static class Builder implements Interceptor.Builder {
-		public Interceptor build() {
+    public static class Builder implements Interceptor.Builder {
+        public Interceptor build() {
             return new CacheableInterceptor();
-		}
+        }
 
-		@Override
-		public void configure(Context context) {
-		}
-	}
+        @Override
+        public void configure(Context context) {
+        }
+    }
 }
